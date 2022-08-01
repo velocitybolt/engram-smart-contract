a580f9eb76ee6b3d0b2d8771c9d71a4ddc5c2d6b
velocitybolt <26785090+velocitybolt@users.noreply.github.com>
2022-08-01 16:27:35 -0500

    v1 of engram contract

---
+++ engramContract.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts@4.4.2/utils/Strings.sol";
+
+contract Engram is ERC721A, Ownable {
+    uint256 MAX_MINTS = 5;
+    uint256 MAX_SUPPLY = 4444;
+    uint256 public mintRate = 0.002 ether;
+
+    string public baseURI = "ipfs://QmdobLjA2JLHhQiGrHi5okmU7PWWa4femKFYbC4RDPXekW/";
+    bool public revealed = false;
+
+    constructor() ERC721A("Engram", "ENG") {}
+
+    function mint(uint256 quantity) external payable {
+        require(quantity + _numberMinted(msg.sender) <= MAX_MINTS, "Exceeded the limit");
+        require(totalSupply() + quantity <= MAX_SUPPLY, "Not enough tokens left");
+        require(msg.value >= (mintRate * quantity), "Not enough ether sent");
+        _safeMint(msg.sender, quantity);
+    }
+
+    function externalMint(uint256 quantity) external payable onlyOwner {
+        require(totalSupply() + quantity <= MAX_SUPPLY, "Not enough tokens left");
+        require(msg.value >= (mintRate * quantity), "Not enough ether sent");
+        _safeMint(msg.sender, quantity);
+    }
+
+    function withdraw() external payable onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    function changeBaseURI(string memory baseURI_) external onlyOwner {
+        baseURI = baseURI_;
+        revealed = true;
+    }
+
+    function setMintRate(uint256 _mintRate) public onlyOwner {
+        mintRate = _mintRate;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        string memory baseURI_ = _baseURI();
+
+        if (revealed) {
+            return bytes(baseURI_).length > 0 ? string(abi.encodePacked(baseURI_, Strings.toString(tokenId), ".json")) : "";
+        } else {
+            return string(abi.encodePacked(baseURI_, "notRevealed.json"));
+        }
+    }
+}
